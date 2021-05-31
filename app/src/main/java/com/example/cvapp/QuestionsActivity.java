6fa39499fe5462e41f2d916740a19ce06daf5788
Vuxajlo <vukmijatovic@gmail.com>
2021-05-31 13:35:00 +0200

    set first question

--- app/src/main/java/com/example/cvapp/QuestionsActivity.java
+++ app/src/main/java/com/example/cvapp/QuestionsActivity.java
@@ -7,13 +7,24 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Collections;
 import java.util.List;
 
 public class QuestionsActivity extends AppCompatActivity {
 
     private QuestionsViewModel questionsViewModel;
+    List<Questions> questionsList;
+    TextView questionsView;
+    Button optionA;
+    Button optionB;
+    Button optionC;
+    Button optionD;
+    Questions currentQuestion;
+
+    int currentQuestionNumber = 1;
 
 
 
@@ -21,25 +32,47 @@ public class QuestionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+        TextView questionNumbers = findViewById(R.id.questionsNumber);
+        questionsView = findViewById(R.id.question_view);
+        optionA = findViewById(R.id.optionA);
+        optionB = findViewById(R.id.optionB);
+        optionC = findViewById(R.id.optionC);
+        optionD = findViewById(R.id.optionD);
+
+
 
         questionsViewModel = new ViewModelProvider(this).get(QuestionsViewModel.class);
         questionsViewModel.getAllQuestions().observe(this, new Observer<List<Questions>>() {
             @Override
             public void onChanged(List<Questions> questions) {
+                questionsList = questions;
+                Collections.shuffle(questionsList);
+                questionNumbers.setText("Question: " + currentQuestionNumber
+                        + "/" + questionsList.size());
+                setQuestion();
 
             }
         });
 
-
-        View button1 = findViewById(R.id.option1);
         AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F);
-        button1.setOnClickListener(new View.OnClickListener() {
+        optionA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button1.startAnimation(buttonClick);
+           
+
+                };
             }
+
         });
 
+    }
 
+    private void setQuestion() {
+        currentQuestion = questionsList.get(currentQuestionNumber);
+        questionsView.setText(currentQuestion.getQuestion());
+        optionA.setText(currentQuestion.getOptionA());
+        optionB.setText(currentQuestion.getOptionB());
+        optionC.setText(currentQuestion.getOptionC());
+        optionD.setText(currentQuestion.getOptionD());
     }
 }
