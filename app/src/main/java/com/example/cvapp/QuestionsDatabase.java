18ac51aaea3838e73b27faea784ca2aca6992478
Vuxajlo <vukmijatovic@gmail.com>
2021-06-11 10:53:55 +0200

    Added more questions

--- app/src/main/java/com/example/cvapp/QuestionsDatabase.java
+++ app/src/main/java/com/example/cvapp/QuestionsDatabase.java
@@ -49,11 +49,30 @@ public abstract class QuestionsDatabase extends RoomDatabase {
                                 "Phoebe", "Ross", "Ross"));
                         questionsDao.insert(new Questions("Oh... My... Good!", "Phoebe", "Gunther",
                                 "Chandler", "Janice", "Janice"));
-                        questionsDao.insert(new Questions("Oh... My... Good!", "Phoebe", "Gunther",
-                                "Chandler", "Janice", "Janice"));
+                        questionsDao.insert(new Questions("The fridge broke so I had to eat everything.", "Phoebe", "Joe",
+                                "Chandler", "Richard", "Joe"));
+                        questionsDao.insert(new Questions("I was wondering if you'd like to go to a movie with me sometime... as my lover.", "Ross", "The Copygirl",
+                                "Gunther", "Joe", "Gunther"));
+                        questionsDao.insert(new Questions("I've got this uncontrollable need to please people.", "Monica", "Phoebe",
+                                "Ross", "Joe", "Monica"));
+                        questionsDao.insert(new Questions("Pants: like shorts but longer.", "Joe", "Jack Geller",
+                                "Chandler", "Phoebe", "Chandler"));
+                        questionsDao.insert(new Questions("Joey doesn’t share food!", "Joe", "Chandler", "Rachel", "Monica", "Joe"));
+                        questionsDao.insert(new Questions("Isn’t that just kick you in the crotch, spit on your neck fantastic?", "Chandler", "Ross",
+                                "Monica", "Rachel", "Rachel"));
+                        questionsDao.insert(new Questions("OK, no uterus, no opinion.", "Monica", "Rachel", "Phoebe", "Caroll", "Rachel"));
+                        questionsDao.insert(new Questions("Gum would be perfection.", "Chandler", "Joe", "Phoebe", "Ross", "Chandler"));
+                        questionsDao.insert(new Questions("I wish I could, but I don’t want to.", "Phoebe", "Ursula", "Ross",
+                                "chandler", "Phoebe" ));
+                        questionsDao.insert(new Questions("I’m hopeless and awkward and desperate for love!", "Monica", "Ross",
+                                "Chandler", "Frank Jr.", "Chandler"));
+                        questionsDao.insert(new Questions("See? He’s her lobster. ", "Phoebe", "Monica", "Rachel", "Janice", "Phoebe"));
+                        questionsDao.insert(new Questions("We better stick to the routine; we don't want to look stupid.", "Ross", "Monica", "Rachel", "Chandler", "Monica"));
+
 
 
                     }
+
                 }
             });
         }
