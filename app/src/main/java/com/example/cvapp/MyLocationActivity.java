7ec24f086bec25735de3eb88f6b1e48658bafff3
Vuxajlo <vukmijatovic@gmail.com>
2021-06-22 11:34:48 +0200

    handled onProviderEnabled

--- app/src/main/java/com/example/cvapp/MyLocationActivity.java
+++ app/src/main/java/com/example/cvapp/MyLocationActivity.java
@@ -1,6 +1,7 @@
 package com.example.cvapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -8,6 +9,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
@@ -16,9 +18,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,12 +36,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.cvapp.databinding.ActivityMyLocationBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MyLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final long LOCATION_UPDATE_INTERVAL = 5000;
+    private static final int REQUEST_CHECK_SETTINGS = 111;
     private GoogleMap googleMap;
     LocationManager locationManager;
     private ActivityMyLocationBinding binding;
@@ -58,18 +73,23 @@ public class MyLocationActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        checkLocationServices();
 
-            ActivityCompat.requestPermissions(this, new String[]
+
+    }
+
+    private void findMe() {
+        if (ActivityCompat.checkSelfPermission(MyLocationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MyLocationActivity.this, new String[]
                             {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
         } else {
-            findMe();
+            setLocationListener();
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
@@ -77,13 +97,13 @@ public class MyLocationActivity extends FragmentActivity implements OnMapReadyCa
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            findMe();
+            setLocationListener();
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
     }
 
-    private void findMe() {
+    private void setLocationListener() {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
@@ -108,29 +128,57 @@ public class MyLocationActivity extends FragmentActivity implements OnMapReadyCa
 
             @Override
             public void onProviderDisabled(@NonNull String provider) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MyLocationActivity.this);
-                builder.setMessage(R.string.enable_location);
-                builder.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                });
-                builder.setNegativeButton("Cancel.", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
+                checkLocationServices();
+            }
 
-                AlertDialog dialog = builder.create();
-                dialog.show();
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+                checkLocationServices();
             }
         };
+    }
 
+    private void checkLocationServices() {
+
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(LOCATION_UPDATE_INTERVAL)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        LocationServices.getSettingsClient(this)
+                .checkLocationSettings(builder.build())
+                .addOnSuccessListener(MyLocationActivity.this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        findMe();
+                    }
+                })
+                .addOnFailureListener(MyLocationActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull @NotNull Exception e) {
+                        if (e instanceof ResolvableApiException) {
+                            try {
+                                ResolvableApiException resolvable = (ResolvableApiException) e;
+                                resolvable.startResolutionForResult(MyLocationActivity.this,
+                                        REQUEST_CHECK_SETTINGS);
+
+                            } catch (IntentSender.SendIntentException sendEx) {
+                                // Ignore the error.
+                            }
+                        }
+
+                    }
+                });
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CHECK_SETTINGS && resultCode == -1) {
+            checkLocationServices();
+        }
+    }
 
     @Override
     protected void onStop() {
